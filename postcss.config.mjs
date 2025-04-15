7ee98c46baf937673136a2ef9f5c21b481177424
Paulo H Motta <paulomotta@customcode.com.br>
2025-04-15 13:41:54 -0300

    first

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,3 +3,19 @@ const config = {
 };
 
 export default config;
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-poppins)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};

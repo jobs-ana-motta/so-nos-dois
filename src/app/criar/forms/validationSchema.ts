7c0380d4c2c4e347192bc1f2dde9211be5cc213f
Paulo H Motta <paulomotta@customcode.com.br>
2025-04-23 16:48:42 -0300

    prview branch

--- src/app/criar/forms/validationSchema.ts
+++ src/app/criar/forms/validationSchema.ts
@@ -1,11 +1,21 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export const validationSchema = Yup.object().shape({
-    nome1: Yup.string().min(3, "Nome muito curto").max(30, "Nome muito longo").required("Nome obrigatório"),
-    nome2: Yup.string().min(3, "Nome muito curto").max(30, "Nome muito longo").required("Nome obrigatório"),
-    data: Yup.date().required("Data obrigatória"),
-    message: Yup.string().min(40, "Mensagem muito curta").required("Mensagem obrigatória"),
-    emoji: Yup.string().required("Emoji obrigatório"),
-    cor: Yup.string().required("Cor obrigatória"),
-    file: Yup.string().required("Foto obrigatória"),
-})
+  nome1: Yup.string()
+    .min(3, "Nome muito curto")
+    .max(30, "Nome muito longo")
+    .required("Nome obrigatório"),
+  nome2: Yup.string()
+    .min(3, "Nome muito curto")
+    .max(30, "Nome muito longo")
+    .required("Nome obrigatório"),
+  data: Yup.date().required("Data obrigatória"),
+  message: Yup.string()
+    .min(40, "Mensagem muito curta")
+    .required("Mensagem obrigatória"),
+  emoji: Yup.string().required("Emoji obrigatório"),
+  cor: Yup.string().required("Cor obrigatória"),
+  file: Yup.array()
+    .of(Yup.string().required("Imagem obrigatória"))
+    .min(1, "Envie pelo menos 1 imagem")
+});

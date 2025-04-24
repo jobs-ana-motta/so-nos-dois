71c7f172a10e8497c0656c8dd70ba85bb58528eb
Paulo H Motta <paulomotta@customcode.com.br>
2025-04-24 08:48:24 -0300

    preview adjustes

--- src/components/imagePreview.tsx
+++ src/components/imagePreview.tsx
@@ -1,28 +1,36 @@
 import Image from "next/image";
-import { Carousel, CarouselContent, CarouselNext, CarouselPrevious } from "./ui/carousel";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
 
 export default function ImagePreview({ src }: { src: string | Array<string> }) {
-    const photos = Array.isArray(src) ? src : [src];
-    return (
-        <Carousel>
-                <CarouselContent 
-                >
-                  {photos.map((url, index) => (
-                    <Image
-                      key={index}
-                      src={url}
-                      alt="Foto de casal"
-                      width={401}
-                      height={640}
-                      quality={100}
-                      className="rounded-2xl object-contain w-full h-auto max-w-[401px]"
-                      priority
-                      sizes="(max-width: 768px) 100vw, 400px"
-                    />
-                  ))}
-                </CarouselContent>
-                <CarouselPrevious/>
-                <CarouselNext />
-        </Carousel>
-    )
+  const photos = Array.isArray(src) ? src : [src];
+  return (
+    <Carousel>
+      <CarouselContent>
+        {photos.map((url, index) => (
+          <CarouselItem key={index} className="flex justify-center">
+          <div className="w-full max-w-[401px] h-auto rounded-xl overflow-hidden">
+            <Image
+              src={url}
+              alt="Foto de casal"
+              width={400}
+              height={640}
+              quality={100}
+              className="object-cover w-full h-auto"
+              priority
+              sizes="(max-width: 768px) 100vw, 400px"
+            />
+          </div>
+        </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
 }

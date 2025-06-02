be503fa6185a0f5423504639bd9a4ae6fb5504d2
Paulo H Motta <paulomotta@customcode.com.br>
2025-06-02 08:03:26 -0300

    change envtype

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,25 +1,28 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
-import admin, { ServiceAccount } from "firebase-admin"
-import serviceAccount from "../../auth.google.json" assert { type: "json" };
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import admin, { ServiceAccount } from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as ServiceAccount),
+    credential: admin.credential.cert({
+      clientEmail: process.env.client_email,
+      projectId: process.env.project_id,
+      privateKey: process.env.private_key?.replace(/\\n/g, "\n"),
+    } as ServiceAccount),
   });
 }
 
 const fireBaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(fireBaseConfig);
-const auth = getAuth(app)
+const auth = getAuth(app);
 signInWithEmailAndPassword(auth, "jobsanamotta@gmail.com", "pauloHenrique*31")
-.then((userCredential) => {
+  .then((userCredential) => {
     console.log("Usuário autenticado:", userCredential.user);
   })
   .catch((error) => {
